a98a8f6de1e0a97771f206379137ee0b05c17a1d
Ilya Khorobrykh <horobryh17@gmail.com>
2022-03-30 11:02:43 +0300

    finished 9th lesson and change readme.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
+      <div>
+          <Technologies/>
+          <Header/>
+      </div>
   );
 }
 
