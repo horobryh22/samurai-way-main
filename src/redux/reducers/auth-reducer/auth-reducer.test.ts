9f5810a50d47814f5a1109a7302d93dd5eb3fb52
Ilya Khorobrykh <horobryh17@gmail.com>
2022-07-03 17:49:24 +0300

    I wrote UNIT test for auth reducer

--- src/redux/reducers/auth-reducer/auth-reducer.test.ts
+++ src/redux/reducers/auth-reducer/auth-reducer.test.ts
@@ -1,53 +1,81 @@
-// import {
-//     authReducer,
-//     AuthUserStateType,
-//     setCurrentAuthUserAC
-// } from './auth-reducer';
-// import {UserProfileType} from '../../../api/api';
-//
-// let initialState: AuthUserStateType
-//
-// beforeEach(() => {
-//
-//     initialState = {
-//         currentAuthUser: {} as UserProfileType,
-//         isAuth: false
-//     }
-// })
-//
-// test('Just profile of current authorized user should shown on the "Profile" page', () => {
-//
-//     const userProfile: UserProfileType = {
-//         aboutMe: 'I like to programming',
-//         userId: 1,
-//         contacts: {
-//             youtube: null,
-//             website: null,
-//             twitter: null,
-//             vk: null,
-//             mainLink: null,
-//             instagram: null,
-//             github: null,
-//             facebook: null
-//         },
-//         fullName: 'Ilya Khororbrykh',
-//         photos: {
-//             large: null,
-//             small: null
-//         },
-//         lookingForAJob: true,
-//         lookingForAJobDescription: 'I want to work in programming sphere'
-//     }
-//
-//     const newState = authReducer(initialState, setCurrentAuthUserAC(userProfile));
-//
-//
-//     expect(Object.keys(newState).length).toBe(3);
-//     expect(Object.keys(newState.currentAuthUser)).toEqual(Object.keys(userProfile));
-//     expect(Object.values(newState.currentAuthUser)).toEqual(Object.values(userProfile));
-//     expect(Object.values(newState.currentAuthUser)[3]).toEqual('Ilya Khororbrykh');
-//     expect(newState.currentAuthUser.photos.small).toBeNull();
-// })
+import {
+    authReducer,
+    AuthUserStateType, removeAuthUserProfileAC, removeUserDataAC, setAuthUserProfileAC, setUserDataAC,
+} from './auth-reducer';
+import {AuthUserDataType, UserProfileType} from '../../../api/api';
 
+let initialState: AuthUserStateType
 
-export {};
+beforeEach(() => {
+
+    initialState = {
+        isAuth: false,
+        authUserData: {} as AuthUserDataType,
+        authUserProfile: {} as UserProfileType
+    }
+})
+
+test('Authorized user data should be set to the state', () => {
+
+    const authUserData: AuthUserDataType = {
+        id: 24040,
+        login: 'horobryh22',
+        email: 'hvi17@yandex.ru'
+    }
+
+    const newState = authReducer(initialState, setUserDataAC(authUserData, true));
+
+    expect(newState.isAuth).toBeTruthy();
+    expect(newState.authUserProfile).toEqual({});
+    expect(newState.authUserData).toEqual({ id: 24040, login: 'horobryh22', email: 'hvi17@yandex.ru'});
+})
+
+test('Authorized user data should be removed from the state', () => {
+
+    const newState = authReducer(initialState, removeUserDataAC());
+
+    expect(newState.isAuth).toBeFalsy();
+    expect(newState.authUserData).toEqual({});
+    expect(newState.authUserProfile).toEqual({});
+})
+
+test('Authorized user profile data should be set to the state', () => {
+
+    const userProfile: UserProfileType = {
+        aboutMe: 'I like to programming',
+        userId: 1,
+        contacts: {
+            youtube: null,
+            website: null,
+            twitter: null,
+            vk: null,
+            mainLink: null,
+            instagram: null,
+            github: null,
+            facebook: null
+        },
+        fullName: 'Ilya Khororbrykh',
+        photos: {
+            large: null,
+            small: null
+        },
+        lookingForAJob: true,
+        lookingForAJobDescription: 'I want to work in programming sphere'
+    }
+
+    const newState = authReducer(initialState, setAuthUserProfileAC(userProfile));
+
+    expect(newState.authUserProfile).toEqual(userProfile);
+    expect(newState.authUserData).toEqual({});
+    expect(newState.isAuth).toBeFalsy();
+
+})
+
+test('Authorized user profile data should be set to the state', () => {
+
+    const newState = authReducer(initialState, removeAuthUserProfileAC());
+
+    expect(newState.authUserProfile).toEqual({});
+    expect(newState.authUserData).toEqual({});
+    expect(newState.isAuth).toBeFalsy();
+})
